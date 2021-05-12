d7bffb910fc44926167e6fbf46d0f127ee3793af
andrewwillette <willette.andrew1846@gmail.com>
2021-05-11 22:11:11 -0500

    updating main, having terminal displaying some destination addresses from sniffing my network interface

--- main.cpp
+++ main.cpp
@@ -37,32 +37,46 @@ struct foo {
     }
 };
 
-int main() {
+std::vector<Packet> getPackets(){
     std::vector<Packet> vt;
-
-    Sniffer sniffer("en0");
-
+    SnifferConfiguration config;
+    config.set_promisc_mode(true);
+    config.set_filter("ip src 192.168.1.2");
+    Sniffer sniffer("en0", config);
     while (vt.size() != 20) {
         vt.push_back(sniffer.next_packet());
     }
+    std::cout << vt.size() << std::endl;
+    return vt;
+}
 
-    for(const auto& packet : vt) {
-        if(packet.pdu() -> find_pdu<IP>()) {
+void drawIPPackets() {
+    // for(Packet packet : getPackets()){
+    //     // ImGui::Text(packet.pdu()->rfind_pdu<IP>().src_addr().to_string().c_str());
+    //     ImGui::Text("Here1");
+    // }
+    // ImGui::Text("IPPacket1");
+    // ImGui::Text("IPPacket2");
+}
 
-        std::cout << "At: " << packet.timestamp().seconds()
-            << " - " << packet.pdu()->rfind_pdu<IP>().src_addr()
-            << std::endl;
-        }
-    }
+auto createScreen() {
 
-    std::cout << IMGUI_CHECKVERSION() << std::endl;
     ImGui::CreateContext();
 
     auto screen = ImTui_ImplNcurses_Init(true);
     ImTui_ImplText_Init();
+    return screen;
+}
+
+void destroyScreen() {}
+
+int main() {
+    auto screen = createScreen();
 
     int nframes = 0;
     float fval = 1.23f;
+
+    auto packets = getPackets();
 
     while (true) {
         ImTui_ImplNcurses_NewFrame();
@@ -72,20 +86,26 @@ int main() {
 
 
         ImGui::SetNextWindowPos(ImVec2(4, 2), ImGuiCond_Once);
-        ImGui::SetNextWindowSize(ImVec2(50.0, 10.0), ImGuiCond_Once);
+        ImGui::SetNextWindowSize(ImVec2(100.0, 100.0), ImGuiCond_Once);
         ImGui::Begin("Hello, world!");
 
+        for(Packet packet : packets){
+            ImGui::Text(packet.pdu()->rfind_pdu<IP>().dst_addr().to_string().c_str());
+            // ImGui::Text(packet.pdu()->rfind_pdu<IP>().id().c_string());
+            // ImGui::Text("Here1");
+        }
         ImGui::Text("NFrames = %d", nframes++);
         // ImGui::Text("Mouse Pos : x = %g, y = %g", ImGui::GetIO().MousePos.x, ImGui::GetIO().MousePos.y);
         // ImGui::Text("Time per frame %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         // ImGui::Text("Float:");
         // ImGui::SameLine();
         // ImGui::SliderFloat("##float", &fval, 0.0f, 10.0f);
-        ImGui::BeginChild("Scrolling");
-        for(int n=0; n < vt.size(); n++) {
-            const char* value = vt[n].pdu()->rfind_pdu<IP>().src_addr().to_string().c_str();
-            ImGui::Text(value);
-        }
+        drawIPPackets();
+
+        // for(int n=0; n < vt.size(); n++) {
+        //     const char* value = vt[n].pdu()->rfind_pdu<IP>().src_addr().to_string().c_str();
+        //     ImGui::Text(value);
+        // }
             
         ImGui::End();
 
